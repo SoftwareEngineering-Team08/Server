088b3e7062df8b9d36060aa90940e60b848e261f
ohhondgi <5ghdrl@naver.com>
2021-11-30 06:16:42 +0900

    chore: Add UserInfo Mapping

---
+++ backend/src/main/kotlin/com/SE/backend/Data/Distance.kt
@@ -0,0 +1,17 @@
+package com.SE.backend.Data
+
+import lombok.Data
+import kotlin.properties.Delegates
+
+// lombok을 이용해 getter와 setter 세팅
+// https://sehajyang.github.io/etc/2019/03/07/kotlin-and-lombok.html/ - 참고
+
+@Data
+class Distance {
+    var did by Delegates.notNull<Int>()
+    lateinit var location:String
+    var stage by Delegates.notNull<Int>()
+    var parameter by Delegates.notNull<Float>()
+}
+
+
