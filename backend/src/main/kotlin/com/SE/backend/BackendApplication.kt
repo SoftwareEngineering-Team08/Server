96ec40dc9d8262956b6ee8d0bf81eadd02378ddd
ohhondgi <5ghdrl@naver.com>
2021-11-30 05:35:11 +0900

    feat: Success Conntection to mysql database

---
+++ backend/src/main/kotlin/com/SE/backend/BackendApplication.kt
@@ -0,0 +1,13 @@
+package com.SE.backend
+
+import com.SE.backend.Data.ShowMapper
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+
+	runApplication<BackendApplication>(*args)
+}
---
+++ backend/src/main/kotlin/com/SE/backend/Controller/MainController.kt
@@ -0,0 +1,26 @@
+package com.SE.backend.Controller
+
+import com.SE.backend.Data.Distance
+import com.SE.backend.Data.ShowMapper
+import org.springframework.web.bind.annotation.ResponseBody
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.lang.Exception
+import kotlin.jvm.Throws
+
+//@Controller
+@RestController
+class MainController {
+
+    @Autowired
+    private lateinit var showMapper: ShowMapper
+
+    @GetMapping("/list", produces = ["application/json"])
+    @ResponseBody
+    @Throws(Exception::class)
+    fun getList(): List<Distance>{
+        return showMapper.getCityList()
+    }
+}
